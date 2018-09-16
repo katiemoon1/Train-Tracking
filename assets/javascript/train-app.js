f8b95ca8af58a4c6d282628a6fba4e85ce8f6de4
Moon Child <kaitline.moon@gmail.com>
2018-09-15 20:28:39 -0400

    Updated the table and added the image card.

---
+++ assets/javascript/train-app.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+
+// Bringing in Firebase
+var config = {
+    apiKey: "AIzaSyCFGxI2aunPoDWYmMtKA9L45MuJ4sFfr0M",
+    authDomain: "train-tracking-application.firebaseapp.com",
+    databaseURL: "https://train-tracking-application.firebaseio.com",
+    projectId: "train-tracking-application",
+    storageBucket: "train-tracking-application.appspot.com",
+    messagingSenderId: "172820875504"
+  };
+  firebase.initializeApp(config);
+
+
+
+});
